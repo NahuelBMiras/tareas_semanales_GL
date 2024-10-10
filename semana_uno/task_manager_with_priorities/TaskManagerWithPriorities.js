e1ab6015bc5c7df10dc1a3e4a004d4931590a74b
NahuelBMiras <nahuelbasailmiras@gmail.com>
2024-10-10 19:28:30 -0300

    cambios en Tasks

--- semana_uno/task_manager_with_priorities/TaskManagerWithPriorities.js
+++ semana_uno/task_manager_with_priorities/TaskManagerWithPriorities.js
@@ -1,4 +1,11 @@
-const tasks = [];
+const Priority = {
+  alta: 1,
+
+  media: 2,
+
+  baja: 3,
+};
+
 let nextId = 1;
 
 class Task {
@@ -10,64 +17,82 @@ class Task {
   priority;
 
   constructor(title, description, dueDate, priority) {
-    this.creationDate = Date.now();
     this.id = nextId++;
+
     this.title = title;
+
     this.description = description;
-    this.dueDate = dueDate;
+
+    this.creationDate = new Date();
+
+    this.dueDate = new Date(dueDate);
+
     this.priority = priority;
   }
 }
 
-const Priority = {
-  alta: 1,
-  media: 2,
-  baja: 3,
-};
-
-const addTask = (title, description, dueDate, priority) => {
-  const newTask = new Task(title, description, dueDate, priority);
-  tasks.push(newTask);
-};
-
-const findTaskIndex = (key, element) => {
-  const index = tasks.findIndex((task) => task[key] === element);
-  return index;
-};
-
-const removeTask = (id, title) => {
-  const deleteTask = title
-    ? findTaskIndex('title', title)
-    : findTaskIndex('id', id);
-
-  if (deleteTask === -1) {
-    return `Task ${deleteTask} not found`;
+class TaskManager {
+  constructor() {
+    this.tasks = [];
   }
-  tasks.splice(deleteTask, 1);
-  return `Task ${deleteTask} deleted successfully`;
-};
 
-const sortTasks = () => {
-  const sortedTasks = tasks.slice().sort((a, b) => {
-    const priorityComparison = Priority[a.priority] - Priority[b.priority];
-    if (priorityComparison === 0) {
-      return new Date(a.dueDate) - new Date(b.dueDate);
+  addTask(title, description, dueDate, priority) {
+    const newTask = new Task(title, description, dueDate, priority);
+    this.tasks.push(newTask);
+  }
+
+  findTaskIndex(key, element) {
+    const index = this.tasks.findIndex((task) => task[key] === element);
+    return index;
+  }
+
+  removeTask(id, title) {
+    const deleteTask = title
+      ? this.findTaskIndex('title', title)
+      : this.findTaskIndex('id', id);
+    if (deleteTask === -1) {
+      return `Task ${deleteTask} not found`;
     }
-    return priorityComparison;
-  });
 
-  return sortedTasks;
-};
+    this.tasks.splice(deleteTask, 1);
 
-addTask('Tarea 1', 'Descripción de la tarea 1', '2024-12-31', 'alta');
-addTask('Tarea 2', 'Descripción de la tarea 2', '2024-11-30', 'baja');
-addTask('Tarea 3', 'Descripción de la tarea 3', '2024-10-15', 'media');
-addTask('Tarea 4', 'Descripción de la tarea 4', '2024-11-15', 'alta');
-addTask('Tarea 5', 'Descripción de la tarea 5', '2024-12-31', 'alta');
-addTask('Tarea 6', 'Descripción de la tarea 6', '2024-11-30', 'baja');
-addTask('Tarea 7', 'Descripción de la tarea 7', '2024-10-15', 'media');
-addTask('Tarea 8', 'Descripción de la tarea 8', '2024-11-15', 'alta');
+    return `Task ${deleteTask} deleted successfully`;
+  }
 
-console.log('Tareas ordenadas:', sortTasks());
-console.log(removeTask(1));
-console.log('Tareas ordenadas:', sortTasks());
+  sortTasks() {
+    return this.tasks.slice().sort((a, b) => {
+      const priorityComparison = Priority[a.priority] - Priority[b.priority];
+
+      if (priorityComparison === 0) {
+        return new Date(a.dueDate) - new Date(b.dueDate);
+      }
+
+      return priorityComparison;
+    });
+  }
+
+  showTaks() {
+    return this.tasks;
+  }
+}
+
+const manager = new TaskManager();
+
+manager.addTask('Estudiar', 'Estudiar para el examen', '2024-10-12', 'alta');
+
+manager.addTask('Salir', 'Salir a caminar', '2024-10-13', 'baja');
+
+manager.addTask('Estudiar', 'Estudiar para el examen', '2024-10-12', 'alta');
+
+manager.addTask('Salir', 'Salir a caminar', '2024-10-13', 'baja');
+
+manager.addTask('Estudiar', 'Estudiar para el examen', '2024-10-12', 'alta');
+
+manager.addTask('Salir', 'Salir a caminar', '2024-10-13', 'baja');
+
+manager.addTask('Estudiar', 'Estudiar para el examen', '2024-10-12', 'alta');
+
+manager.addTask('Salir', 'Salir a caminar', '2024-10-13', 'baja');
+console.log(manager.showTaks());
+console.log(manager.sortTasks());
+console.log(manager.removeTask(1));
